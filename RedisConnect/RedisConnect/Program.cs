d62d5ba89b9f80459757e4d8c19886a8e70ab2c5
Atanas Kambitov <atanas.kambitov@sainsburys.co.uk>
2021-05-06 15:13:18 +0100

    adding more progects

---
+++ RedisConnect/RedisConnect/Program.cs
@@ -0,0 +1,34 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDatabase database = lazyConnection.Value.GetDatabase();
+
+            Console.WriteLine(database.StringGet("Employee"));
+            database.StringSet("Employee", "Hey This is an employee");
+            Console.WriteLine(database.StringGet("Employee"));
+
+            Console.ReadLine();
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection =
+            new Lazy<ConnectionMultiplexer>(() =>
+            {
+                string connectionString = "codewithatanas.redis.cache.windows.net:6380,password=odFa0uOhgwlTkcgxsc2hHsaTmSQabMrV36HV6jh8L8Q=,ssl=True,abortConnect=False";
+                return ConnectionMultiplexer.Connect(connectionString);
+            });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
---
+++ RedisConnection/RedisConnection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDatabase database = lazyConnection.Value.GetDatabase();
+
+            //Console.WriteLine(database.StringGet("Employee"));
+            //database.StringSet("Employee", "Hey This is an employee");
+            //Console.WriteLine(database.StringGet("Employee"));
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection =
+            new Lazy<ConnectionMultiplexer>(() => 
+            {
+                string connectionString = "codewithatanas.redis.cache.windows.net:6380,password=odFa0uOhgwlTkcgxsc2hHsaTmSQabMrV36HV6jh8L8Q=,ssl=True,abortConnect=False";
+                return ConnectionMultiplexer.Connect(connectionString);
+            });
+
+        public static ConnectionMultiplexer Connection 
+        {
+            get 
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
